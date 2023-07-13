6c5d262b57cc9b5554f974f231f6eaa51416a297
Paulina Stehlik <s2997324@staff.ad.griffith.edu.au>
2023-07-13 15:10:26 +1000

    Age and histogram
    created dummy cohort with age column
    created a histogram of ages.

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,7 +7,24 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
+
+    #"I'm interested in all patients who have never changed practice, between these two dates"
     population=patients.registered_with_one_practice_between(
         "2019-02-01", "2020-02-01"
     ),
+
+    age  = patients.age_as_of(
+        "2019-09-01", #"Give me a column of data corresponding to the age of each patient on the given date"
+        
+        #"I expect every patient to have a value, and the distribution of ages to match that of the real UK population"
+        return_expectations = {
+            "rate" : "universal",
+            "int": {"distribution": "population_ages"},
+        }
+    )
+
+
+
 )
+
+
